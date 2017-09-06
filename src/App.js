de8d93b50df1afb533d6e2588887f57fb7a328be
William Elliott <wcelliott1991@gmail.com>
2017-09-06 11:56:09 -0400

    creating routes by hand

--- src/App.js
+++ src/App.js
@@ -6,21 +6,55 @@ import Jank from './jank.js'
 import Bob from './bob.js'
 import Thing from './thing.js'
 
+const navs = [
+  <Jank />,
+  <Bob />,
+  <Thing />
+]
+
+const navNames = [
+  'Jank',
+  'Thing',
+  'Bob'
+]
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentNav: 0
+    }
+  }
+
   render() {
+    const componentToRender = navs[this.state.currentNav];
+    const myNavs = navs.map((comp, idx) => (
+         <li><a href="#" onClick={(event) => {
+            event.preventDefault();
+            this._changeNav(idx);
+            }}>{navNames[idx]}</a></li>
+    ));
+
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Think Bob Jank Face</h2>
         </div>
-
-        <Jank />
-        <Bob />
-        <Thing />
-      
+        <ul>
+          {myNavs}
+        </ul>
+        {componentToRender}
       </div>
     );
+  }
+
+  _changeNav = (navValue) => {
+    console.log(`You clicked a thing: ${navValue}!`);
+    this.setState({
+      currentNav: navValue
+    })
   }
 }
 
